565332703351862ee070ea810ffa34886d6cb7c9
agahafiz04 <agahafiz04@gmail.com>
2024-01-30 16:56:48 +0700

    Refactor and complete the game board logic
    Added full slotCheck functionality to check the available slot
    
    Added isComplete functionality to check if the game is completed

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/extensions */
+
 import * as util from "util";
-// eslint-disable-next-line import/extensions
 import Ship from "./battle-ship.js";
 
 export default class Gameboard {
@@ -23,17 +25,9 @@ export default class Gameboard {
   }
 
   placeShip([corX, corY], ship) {
-    const checkArray = this.board[corX].length + ship.length + corY;
-
-    for (let i = 0; i < this.board[corX]; i += 1) {
-      console.log(this.board[i]);
-    }
-
-    if (checkArray > 20 || checkArray < 11) return;
-
-    if (this.slotCheck([corX, corY])) {
+    if (this.slotCheck([corX, corY], ship)) {
       for (let i = 0; i < ship.length; i += 1) {
-        this.board[corX].splice(corY + i, 1, [ship, { destroyed: false }]);
+        this.board[corX].splice(corY + i, 1, [ship, "Hit Me"]);
       }
     }
   }
@@ -43,12 +37,12 @@ export default class Gameboard {
 
     switch (typeof shipObject) {
       case "object":
-        if (shipObject[1].destroyed === true) {
+        if (shipObject[1] === "Hit!") {
           break;
         }
 
         shipObject[0].hit();
-        shipObject[1].destroyed = true;
+        shipObject[1] = "Hit!";
         break;
 
       case "number":
@@ -60,29 +54,66 @@ export default class Gameboard {
     }
   }
 
-  slotCheck([corX, corY]) {
+  slotCheck([corX, corY], ship) {
     const shipObject = this.board[corX][corY];
+    const checkArray = this.board[corX].length + ship.length + corY;
+    const currentArray = this.board[corX];
 
-    if (typeof shipObject !== "number") return false;
-    if (typeof shipObject === "object") return false;
+    for (let i = corY; i < corY + ship.length; i += 1) {
+      if (typeof currentArray[i] !== "number") return false;
+    }
+
+    if (typeof currentArray[corY - 1] === "object") return false;
+
+    if (this.board[corX - 1].some((value) => typeof value === "object")) {
+      return false;
+    }
+
+    if (
+      checkArray > 20 ||
+      checkArray < 11 ||
+      typeof shipObject !== "number" ||
+      typeof shipObject === "object"
+    ) {
+      return false;
+    }
 
     return true;
   }
 
-  // isComplete() {
-  //   this.board;
-  // }
+  isComplete() {
+    const shipList = [];
+
+    this.board.forEach((item) => {
+      const getShip = item.filter((ship) => typeof ship === "object");
+      getShip.forEach((ship) => shipList.push(ship[0]));
+    });
+
+    const unique = shipList.filter(
+      (obj, index) => index === shipList.findIndex((o) => obj.name === o.name)
+    );
+
+    const isComplete = unique.every((obj) => obj.sunk === true);
+
+    return isComplete;
+  }
 }
 
 const test = new Gameboard();
-const ship = new Ship(5);
+const ship1 = new Ship(5, "ship-one");
+const ship4 = new Ship(3, "ship-two");
+const ship5 = new Ship(2, "ship-three");
 
-test.placeShip([1, 1], ship);
-test.placeShip([1, 2], ship);
-test.placeShip([1, 0], ship);
+test.placeShip([1, 1], ship1);
+// test.placeShip([1, 7], ship5);
+// test.placeShip([3, 0], ship4);
+// test.placeShip([2, 1], ship2);
 
-// test.receiveAttack([1, 2]);
-// test.receiveAttack([1, 3]);
+test.receiveAttack([1, 1]);
+test.receiveAttack([1, 2]);
+test.receiveAttack([1, 3]);
+test.receiveAttack([1, 4]);
+test.receiveAttack([1, 5]);
 // test.receiveAttack([1, 4]);
 // test.receiveAttack([1, 5]);
 // test.receiveAttack([0, 1]);
@@ -95,4 +126,8 @@ test.placeShip([1, 0], ship);
 // test.receiveAttack([0, 1]);
 // test.receiveAttack([0, 2]);
 
-// console.log(util.inspect(test.board, false, null, true));
+console.log(util.inspect(test.board, false, null, true));
+
+console.log(test.isComplete());
+
+// 1.
--- src/js/player.js
+++ src/js/player.js
@@ -1,1 +1,10 @@
-export default class Player {}
+export default class Player {
+  constructor(playerName) {
+    this.player = playerName;
+  }
+}
+
+const playerOne = new Player("Player One");
+// const playerTwo = new Player("Computer");
+
+console.log(playerOne);
